bd8b887547932784d9209094811e5c93c8c50067
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-21 15:05:35 -0300

    beryllium: Implement libinit
    * Use pixel fingerprint to bypass safetynet

--- citrus_beryllium.mk
+++ citrus_beryllium.mk
@@ -16,11 +16,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Poco F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:8.1.0/OPM1.171019.026/V10.0.4.0.OEJMIFH:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 8.1.0 OPM1.171019.026 V10.0.4.0.OEJMIFH release-keys" \
     PRODUCT_NAME="beryllium" \
     TARGET_DEVICE="beryllium"
 
+		
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
